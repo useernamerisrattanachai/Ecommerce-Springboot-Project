a2b91bf08d79a4c2842db20cb46041b9a85b00e0
rattanachaikku <110150336+rattanachaikku@users.noreply.github.com>
2023-05-12 14:28:52 +0700

    Set-up Customer Module | Part 10  finished !

---
+++ Library/src/main/java/com/ecommerce/library/model/CartItem.java
@@ -0,0 +1,29 @@
+package com.ecommerce.library.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name="cart_item")
+public class CartItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "order_detail_id")
+    private Long id;
+    private int quantity;
+    private double totalPrice;
+
+    @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinColumn(name = "shopping_cart_id", referencedColumnName = "shopping_cart_id")
+    private ShoppingCart cart;
+
+    @OneToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "product_id", referencedColumnName = "product_id")
+    private Product product;
+}
